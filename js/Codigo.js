27ec69c51af7230c170a6eaba78f80f0cf44cef7
carlflex <Ricardogugliermino@gmail.com>
2022-10-24 15:08:27 -0300

    true=blanco/false=negro

--- js/Codigo.js
+++ js/Codigo.js
@@ -1,9 +1,22 @@
 $("Document").ready(function(){
     const D20Normal = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19, 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19, 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20, 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20]
-    
+    var Dadopara = true
+
+    function fncCambioDado(){
+        Dadopara = !Dadopara
+        console.log(Dadopara)
+        if(Dadopara){
+            $("#Dadopara").attr("class","blanco")
+        }else{
+            $("#Dadopara").attr("class","negro")
+        }
+    }
+
     function fncDado(){
         var Evento = D20Normal[Math.floor(Math.random()*78)]
         $("#Eventoimg").attr("src","img/Eventos/"+Evento+".png")
+
+        fncCambioDado();
     }
 
     $("#BtnDado").on("click",fncDado);
