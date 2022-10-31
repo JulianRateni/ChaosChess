9c3cae82020c1bf343eb16409d157d7cb576c4d3
carlflex <Ricardogugliermino@gmail.com>
2022-10-31 15:06:43 -0300

    movimiento peones

--- js/Codigo.js
+++ js/Codigo.js
@@ -1,4 +1,5 @@
 
+
 $("Document").ready(function(){
     const D20Normal = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19, 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19, 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20, 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20]
     var Dadopara = true
@@ -21,6 +22,11 @@ $("Document").ready(function(){
   */
   var y = 0
   var x = 0
+  var htmlPieza1
+  var htmlPieza2
+  var piezaSel
+  var piezaID
+  var idIntervalo
 
     function renderTablero(){
          for(y = 0; y < 8; y++){
@@ -39,16 +45,24 @@ $("Document").ready(function(){
     var tiempo = parseInt($("#Reloj").val())
 
     $(".espacio").on("click",function(){
-        $(".resalto").removeClass("resalto")
-        $(".capturable").removeClass("capturable")
         y = $(this).attr("id").slice(0,1)
         x = $(this).attr("id").slice(-1)
-       var piezaSel = $(this).html().slice(10,13)
-       console.log(piezaSel+" "+y+x)
-       if(piezaSel.slice(0,2)!="nd"){
-         if(piezaSel.slice(1,2) == turnos[deQuien].slice(0,1)){
-             switch(piezaSel.slice(0,1)){
-                 case "P": movPeon(piezaSel.slice(1,3),y,x); break;
+        if($(this).hasClass("resalto") || $(this).hasClass("capturable")){
+            htmlPieza2 = "#"+$(this).attr("id")
+            console.log(htmlPieza2)
+            moverOCapturar(htmlPieza1,piezaSel,htmlPieza2);
+            return;
+        }
+        $(".resalto").removeClass("resalto")
+        $(".capturable").removeClass("capturable")
+        htmlPieza1 = "#"+$(this).attr("id")
+       piezaSel = $(this).html()
+       piezaID = $(this).html().slice(10,13)
+       console.log(piezaID+" "+y+x)
+       if(piezaID.slice(0,2)!="nd"){
+         if(piezaID.slice(1,2) == turnos[deQuien].slice(0,1)){
+             switch(piezaID.slice(0,1)){
+                 case "P": movPeon(piezaID.slice(1,3),y,x); break;
                  case "C": movCaballo(y,x); break;
                  case "A": movAlfil(y,x); break;
                  case "T": movTorre(y,x); break;
@@ -59,54 +73,72 @@ $("Document").ready(function(){
        }
      })
 
+     function moverOCapturar(OGPos,pieza,SeMueveOCapturaA){
+        $(OGPos).empty().html()
+        console.log(piezaID.slice(1,3).slice(-1))
+        if(piezaID.slice(1,3).slice(-1) == "C"){
+            $(SeMueveOCapturaA).empty().html(pieza.slice(0,12)+`" `+pieza.slice(14,33)+`.png">`)
+        }else{
+            $(SeMueveOCapturaA).empty().html(pieza)
+        }
+        pieza.empty
+        $(".resalto").removeClass("resalto")
+        $(".capturable").removeClass("capturable")
+        tiempoFin(true);
+     }
+
      function movPeon(tipo,y,x){
         console.log("#"+(y-(-1))+x)
         if(tipo.slice(0,1) == "B"){
             if($("#"+(y-1)+x).html()==` `){
                 $("#"+(y-1)+x).addClass("resalto")
-            }else{
-                $("#"+(y-1)+x).addClass("capturable")
             }
             if(tipo.slice(-1) == "C"){
                 if($("#"+(y-2)+x).html()==` `){
                     $("#"+(y-2)+x).addClass("resalto")
-                }else{
-                    $("#"+(y-2)+x).addClass("capturable")
                 }
+            }
+            if(!($("#"+(y-1)+(x-1)).html()== ` `)){
+                $("#"+(y-1)+(x-1)).addClass("capturable")
+            }
+            if(!($("#"+(y-1)+(x-(-1))).html()== ` `)){
+                $("#"+(y-1)+(x-(-1))).addClass("capturable")
             }
         }
         if(tipo.slice(0,1) == "N"){
             if($("#"+(y-(-1))+x).html()==` `){
                 $("#"+(y-(-1))+x).addClass("resalto")
-            }else{
-                $("#"+(y-(-1))+x).addClass("capturable")
             }
             if(tipo.slice(-1) == "C"){
                 if($("#"+(y-(-2))+x).html()==` `){
                     $("#"+(y-(-2))+x).addClass("resalto")
-                }else{
-                    $("#"+(y-(-2))+x).addClass("capturable")
                 }
+            }
+            if(!($("#"+(y-(-1))+(x-1)).html()==` `)){
+                $("#"+(y-(-1))+(x-1)).addClass("capturable")
+            }
+            if(!($("#"+(y-(-1))+(x-(-1))).html()==` `)){
+                $("#"+(y-(-1))+(x-(-1))).addClass("capturable")
             }
         }
      }
-     function movCaballo(){
+     function movCaballo(y,x){
 
 
      }
-     function movAlfil(){
+     function movAlfil(y,x){
 
 
      }
-     function movTorre(){
+     function movTorre(y,x){
 
 
      }
-     function movDama(){
+     function movDama(y,x){
 
 
      }
-     function movRey(){
+     function movRey(y,x){
 
 
      }
@@ -146,15 +178,21 @@ $("Document").ready(function(){
         ComienzoTurno();
     }
 
+    function tiempoFin(jugadorJugo){
+        clearInterval(idIntervalo);
+        if(!jugadorJugo){
+            tiempo = `:(`
+        }
+        $("#Reloj").val(tiempo)
+        setTimeout(FinTurno,100);
+    }
+
     function TurnoJugador(Color){
         $("#TurnoDe").val("Turno de: "+Color);
         setTimeout(function(){
-            let idIntervalo = setInterval(function(){
+             idIntervalo = setInterval(function(){
                 if(tiempo <= 0){
-                    clearInterval(idIntervalo);
-                    tiempo = `:(`
-                    $("#Reloj").val(tiempo)
-                    setTimeout(FinTurno,100);
+                    tiempoFin(false);
                 }
                 else{
                     tiempo -= 0.01
